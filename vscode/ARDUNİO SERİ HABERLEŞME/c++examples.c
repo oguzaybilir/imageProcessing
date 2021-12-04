2533abaaa9bd3eb5c94343d456b2a5f57ec20c86
Yusuf Oğuzay <93535901+oguzaybilir@users.noreply.github.com>
2021-12-04 17:08:31 +0300

    helalkje

--- vscode/ARDUNİO SERİ HABERLEŞME/c++examples.c
+++ vscode/ARDUNİO SERİ HABERLEŞME/c++examples.c
@@ -1,3 +1,12 @@
+/*Karar 1*/
 #include <stdio.h>
 #include <conio.h>
-#include <math.h>
+int main(void){
+	int a;
+	printf("Bir tamsayi giriniz:"); scanf("%d",&a);
+	if(a>0) printf("Pozitif");
+	else if(a<0) printf("Negatif");
+	else printf("Sifir");
+	getch();
+	return 0;
+}
