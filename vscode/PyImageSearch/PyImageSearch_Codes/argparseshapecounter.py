9e98f168fb56e3986a8fd96b1824c4b9fc07d52f
Yusuf Oğuzay <93535901+oguzaybilir@users.noreply.github.com>
2021-12-17 19:58:32 +0300

    some changes

--- vscode/PyImageSearch/PyImageSearch_Codes/argparseshapecounter.py
+++ vscode/PyImageSearch/PyImageSearch_Codes/argparseshapecounter.py
@@ -11,6 +11,7 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["input"])
 
 gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+cv2.imshow("image",gray)
 blur = cv2.GaussianBlur(gray,(5,5),0)
 thresh = cv2.threshold(blur, 60,255,cv2.THRESH_BINARY)[1]
 
@@ -34,3 +35,5 @@ cv2.imwrite(args["output"],image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows
+
+
